ae8d599701a439ad736d15e69ca614f326dcf278
nampdn <nampdn@protonmail.com>
2018-12-23 13:30:01 +0700

    build(components): setting up components package
    #10

---
+++ packages/components/src/screens/HomeScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-primitives';
+
+export const HomeScreen = React.memo(() => {
+  return (
+    <View>
+      <Text>Hello Todo App</Text>
+    </View>
+  );
+});
