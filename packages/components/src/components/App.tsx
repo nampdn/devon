02aff74f641586cad441db6dc52045a116acf9ed
nampdn <nampdn@protonmail.com>
2018-12-24 17:10:34 +0700

    chore(mobile): setting up project

---
+++ packages/components/src/components/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export const App = () => (
+  <View>
+    <Text>DEVON</Text>
+    <Text>Build apps without gaps!</Text>
+  </View>
+);
